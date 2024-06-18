92bc56e9eb7d389ebea3696ee36191751bd7eda6
mariana <marianamayaya@gmail.com>
2024-06-18 16:44:56 -0500

    Cambios en el aplicativo movil

--- aplicativoMovil/app/build.gradle.kts
+++ aplicativoMovil/app/build.gradle.kts
@@ -37,6 +37,12 @@ android {
 
 dependencies {
 
+    //dependencias para hacer peticiones restFull
+    implementation("com.android.volley:volley:1.2.1")
+    //tareas en segundo hilo
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
+    //para interpretar la respuesta JSON
+    implementation("com.google.code.gson:gson:2.10.1")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
