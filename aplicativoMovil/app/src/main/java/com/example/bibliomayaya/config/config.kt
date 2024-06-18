a128f9b1712ab3af03163c5e25522ee5ace8618e
mariana <marianamayaya@gmail.com>
2024-06-18 09:04:59 -0500

    Cambios en el aplicativo movil

---
+++ aplicativoMovil/app/src/main/java/com/example/bibliomayaya/config/config.kt
@@ -0,0 +1,11 @@
+package com.example.bibliomayaya.config
+
+class config {
+
+    //se crea una url static, para consultar sin instanciar
+    //método companion object sirve para almacenar las variables static
+
+    companion object{
+        val urlBase="http://192.168.137.167:8080/"
+    }
+}
